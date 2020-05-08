c302f21cc195f510e195a4c244030b1df7625c0a
Thapelo.Mokole <Thapelo.Mokole@psg.co.za>
2020-05-08 22:50:13 +0200

    end points

--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,9 @@
+// Modules
 export * from './core/app/modules/pdf-create/pdf-create.module';
+
+// Services
+export * from './core/app/services/pdf-create.service';
+
+// Models
+export * from './core/app/models/pdf';
+export * from './core/app/models/table';
