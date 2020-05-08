180a300aaad719571238306ae81a6665b4b51cac
Thapelo.Mokole <Thapelo.Mokole@psg.co.za>
2020-05-08 22:23:36 +0200

    tsconfig changes

---
+++ core/app/index.ts
@@ -0,0 +1,9 @@
+// Services
+export { PdfCreateService } from './services/pdf-create.service';
+
+// Modules
+export { PdfCreateModule } from './modules/pdf-create/pdf-create.module';
+
+// Models
+export * from './models/table';
+export * from './models/pdf';
