bcae22c768c1607ebcffc25bd2d62552c7984cee
Thapelo.Mokole <Thapelo.Mokole@psg.co.za>
2020-05-09 06:43:29 +0200

    expose all endpoints

--- public_api.ts
+++ public_api.ts
@@ -2,8 +2,8 @@
 export * from './core/app/modules/angular-boss-pdf/angular-boss-pdf.module';
 
 // Services
-// export * from './core/app/services/angular-boss-pdf.service';
+export * from './core/app/services/angular-boss-pdf.service';
 
 // Models
-// export * from './core/app/models/pdf';
-// export * from './core/app/models/table';
+export * from './core/app/models/pdf';
+export * from './core/app/models/table';
